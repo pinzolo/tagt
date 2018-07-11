d51e1bb6edfe606188f2ce5fabc16a905c37dc86
pinzolo <pinzolo@gmail.com>
2018-07-11 19:16:12 +0900

    Implements

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# tagt
-tagt is test helper that adds tags to error message.
+# tagt: Friend of table driven tests
+tagt is test helper that prints tags with original error message.
+
+## Usage
+
+```go
+testdata := []struct{
+	id  int
+	exists bool
+	tag string
+}{
+	{1, true, "valid user"},
+	{9, false, "unknown user"},
+}
+for _, data := range testdata {
+	tt := tagt.New(t, data.tag)
+	user := repo.FindByID(data.id)
+	if d.exists && user == nil {
+		t.Errorf("user should be found by id is %d", data.id)
+		// => user should be found by id is 1
+		tt.Errorf("user should be found by id is %d", data.id)
+		// => [valid user] user should be found by id is 1
+	} else if !d.exists && user != nil {
+		t.Errorf("user should not be found by id is %d", data.id)
+		// => user should not be found by id is 9
+		tt.Errorf("user should not be found by id is %d", data.id)
+		// => [unknown user] user should not be found by id is 9
+	}
+}
+```
--- tagt.go
+++ tagt.go
@@ -1,4 +1,134 @@
 package tagt
 
-type T stuct {
+import (
+	"fmt"
+	"testing"
+)
+
+// T prints tags when test funcs are called.
+type T struct {
+	t    *testing.T
+	tags []string
+}
+
+// New T from testing.T and tags.
+func New(t *testing.T, tags ...string) T {
+	return T{
+		t:    t,
+		tags: tags,
+	}
+}
+
+// Error prints tags with original function.
+func (t T) Error(args ...interface{}) {
+	t.t.Helper()
+	t.t.Error(t.newArgs(args)...)
+}
+
+// Errorf prints tags with original function.
+func (t T) Errorf(format string, args ...interface{}) {
+	t.t.Helper()
+	if len(args) == 0 {
+		t.Error(format)
+	} else {
+		t.t.Error(t.tag(), fmt.Sprintf(format, args))
+	}
+}
+
+// Fail delegates original function.
+func (t T) Fail() {
+	t.t.Helper()
+	t.t.Fail()
+}
+
+// FailNow delegates original function.
+func (t T) FailNow() {
+	t.t.Helper()
+	t.t.FailNow()
+}
+
+// Failed delegates original function.
+func (t T) Failed() bool {
+	return t.t.Failed()
+}
+
+// Fatal prints tags with original function.
+func (t T) Fatal(args ...interface{}) {
+	t.t.Helper()
+	t.t.Fatal(t.newArgs(args)...)
+}
+
+// Fatalf prints tags with original function.
+func (t T) Fatalf(format string, args ...interface{}) {
+	t.t.Helper()
+	if len(args) == 0 {
+		t.Fatal(format)
+	} else {
+		t.t.Fatal(t.tag(), fmt.Sprintf(format, args))
+	}
+}
+
+// Log prints tags with original function.
+func (t T) Log(args ...interface{}) {
+	t.t.Helper()
+	t.t.Log(t.newArgs(args)...)
+}
+
+// Logf prints tags with original function.
+func (t T) Logf(format string, args ...interface{}) {
+	t.t.Helper()
+	if len(args) == 0 {
+		t.Log(format)
+	} else {
+		t.t.Log(t.tag(), fmt.Sprintf(format, args))
+	}
+}
+
+// Name delegates original function.
+func (t T) Name() string {
+	return t.t.Name()
+}
+
+// Skip prints tags with original function.
+func (t T) Skip(args ...interface{}) {
+	t.t.Helper()
+	t.t.Skip(t.newArgs(args)...)
+}
+
+// SkipNow delegates original function.
+func (t T) SkipNow() {
+	t.t.Helper()
+	t.SkipNow()
+}
+
+// Skipf prints tags with original function.
+func (t T) Skipf(format string, args ...interface{}) {
+	t.t.Helper()
+	if len(args) == 0 {
+		t.Skip(format)
+	} else {
+		t.t.Skip(t.tag(), fmt.Sprintf(format, args))
+	}
+}
+
+// Skipped delegates original function.
+func (t T) Skipped() bool {
+	return t.t.Skipped()
+}
+
+func (t T) tag() string {
+	tg := ""
+	for _, v := range t.tags {
+		tg += "[" + v + "]"
+	}
+	return tg
+}
+
+func (t T) newArgs(args []interface{}) []interface{} {
+	ret := make([]interface{}, len(args)+1)
+	ret[0] = t.tag()
+	for i, v := range args {
+		ret[i+1] = v
+	}
+	return ret
 }
